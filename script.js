ff37cdfd2a45c4f63c1003a88d6231659c815202
George Apraksin <dreamvibe@mail.ru>
2022-05-11 14:09:16 +0500

    funny generator:

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 const gridContainer = document.querySelector(".grid-container");
 
-for (let i = 0; i < 64 * 64; i++) {
-  if (Math.ceil(Math.random(0, 10) * 10) % 7 === 0) {
-    gridContainer.insertAdjacentHTML("afterbegin", "<div class='grid-cell yellow' onclick='openBurialSheet()'></div>");
-  } else {
-    gridContainer.insertAdjacentHTML("afterbegin", "<div class='grid-cell'></div>");
+const giveMeRandomInt = (min, max) => {
+  const r = Math.ceil(Math.random(0, 10) * 10);
+  if (!min || !max) {
+    console.warn("You forgot to pass min and max to random int generator. Returning default");
+    return r;
+  }
+  return r > max ? giveMeRandomInt(min, max) : r < min ? giveMeRandomInt(min, max) : r;
+};
+const exp = () => giveMeRandomInt() % 7 === 0;
+
+function populateYard(useI) {
+  for (let i = 0; i < 64 * 64; i++) {
+    if (useI ? i % useI === 0 : exp()) {
+      gridContainer.insertAdjacentHTML(
+        "afterbegin",
+        "<div class='grid-cell yellow' onclick='openBurialSheet()'></div>"
+      );
+    } else {
+      gridContainer.insertAdjacentHTML("afterbegin", "<div class='grid-cell'></div>");
+    }
   }
 }
+
+function evictYard() {
+  gridContainer.replaceChildren([]);
+  const funnyNumbers = [3, 5, 7, 9, 11, 2, 1, 10];
+  const rand = giveMeRandomInt(0, funnyNumbers.length);
+  console.log(rand);
+  populateYard(funnyNumbers[rand]);
+}
+
+populateYard();
 
 const form = document.querySelector(".grave-id-form");
 const burialSheet = document.querySelector(".burial-sheet");
